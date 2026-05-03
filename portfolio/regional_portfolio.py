d0955e95f1b54318f98dc1ebda9baea9098c9043
Jack Olivieri <jolivieri@anaconda.com>
2026-05-03 16:05:28 +0200

    make title more specific

--- portfolio/regional_portfolio.py
+++ portfolio/regional_portfolio.py
@@ -21,12 +21,12 @@ class RegionalPortfolio(Portfolio):
         self._dmem_visualizer = PieChart(data={
             "Developed Markets": developed_share,
             "Emerging Markets": 1.0 - developed_share,
-        }, title="Developed Markets vs. Emerging Markets")
+        }, title="{}: Developed Markets vs. Emerging Markets".format(self._name))
 
         self._usavn_visualizer = PieChart(data={
             "US": us_within_developed,
             "Non-US": 1.0 - us_within_developed,
-        }, title="US vs. Non-US (within developed markets)")
+        }, title="{}: US vs. Non-US (within developed markets)".format(self._name))
 
         # now let's look at regional split: us vs. non-us vs. emerging markets
         # Scale us_within_developed by the developed_share so that US is proportional to the total_value
@@ -37,7 +37,7 @@ class RegionalPortfolio(Portfolio):
         }
         self._regional_split_visualizer = PieChart(
             data=self._regional_split,
-            title="Total Regional Split (US vs. Non-US vs. EM): {:.2f} Euro".format(self._value),
+            title="{}: Total Regional Split (US vs. Non-US vs. EM): {:.2f} Euro".format(self._name, self._value),
             factor={"value": self._value, "unit": "Euro"},
         )
 
