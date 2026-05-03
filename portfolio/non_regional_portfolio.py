6363aa3f209df5da76eb2af6a2d64f590a9d3331
Jack Olivieri <jolivieri@anaconda.com>
2026-05-03 15:06:25 +0200

    give option to consolidate visualization of non-regional

--- portfolio/non_regional_portfolio.py
+++ portfolio/non_regional_portfolio.py
@@ -3,12 +3,21 @@ from visual.pie_chart import PieChart
 
 
 class NonRegionalPortfolio(Portfolio):
-    def __init__(self, name: str, positions: list[dict]):
+    def __init__(self, name: str, positions: list[dict], consolidate: bool = False):
         super().__init__(name, positions)
 
-        self._visualer_data = {
-            name: self._value,
-        }
+        if consolidate:
+            self._visualer_data = {
+                name: self._value,
+            }
+        else:
+            self._visualer_data = {}
+            for position in self._positions:
+                prev_val = 0
+                label = position._short_name or position._name
+                if label in self._visualer_data:
+                    prev_val = float(self._visualer_data[label])
+                self._visualer_data[label] = prev_val + float(position.value)
 
         self._visualizer = PieChart(
             data=self._visualer_data,
