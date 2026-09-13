3031804c04a95bfd707957c246607e42cce30bed
Jack Olivieri <jolivieri@anaconda.com>
2026-09-13 18:14:22 +0200

    change default visualizer

--- visual/__init__.py
+++ visual/__init__.py
@@ -2,6 +2,6 @@ from .visual import Visual
 from .web_chart import WebChart
 from .pie_chart import PieChart
 
-DEFAULT_VISUALIZER = PieChart
+DEFAULT_VISUALIZER = WebChart
 
 __all__ = ["Visual", "WebChart", "PieChart", "DEFAULT_VISUALIZER"]
